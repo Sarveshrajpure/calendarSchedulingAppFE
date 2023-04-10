0b3914008f9dcc7284c3700de02668a2db16a0ed
sarvesh rajpure <rajpuresarvesh@gmail.com>
2023-04-10 17:12:33 +0530

    Fixes a mistake with axios instance

--- src/utilities/axiosInstance.js
+++ src/utilities/axiosInstance.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const localUrl = "http://localhost:3000/api";
 const prodUrl = "https://calendar-scheduling-app-backend.onrender.com/api";
 
-export const instance = axios.create({
-  baseURL: prodUrl,
+export const axiosInstance = axios.create({
+  baseURL: localUrl,
   timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
 });
